62e318331c5a7791b3527516f700309c15274fa9
Henry Tadeusiak <htadeusiak1@gmail.com>
2026-03-03 18:18:44 -0600

    Allow copies of TireModel class

--- include/tire_model/tire_model.hpp
+++ include/tire_model/tire_model.hpp
@@ -1,32 +1,25 @@
 #pragma once
 
 #include "tire_model/tire_types.hpp"
-#include <memory>
+#include "tire_model/internal/tire_mf612.hpp"
 #include <string>
+#include <variant>
 
 namespace tire_model {
-namespace internal {
-    class TireModelBase;
-}
 
 /**
  * @brief Main tire model class
- * 
+ *
  * Automatically selects and instantiates the appropriate tire model from a .TIR file.
  */
 class TireModel {
 public:
     explicit TireModel(const std::string& filename);
-    
-    ~TireModel();
-
-    TireModel(const TireModel&) = delete;
-    TireModel& operator=(const TireModel&) = delete;
 
     TireForces evaluate(const TireInput& input) const;
-    
+
 private:
-    std::unique_ptr<internal::TireModelBase> model_;
+    std::variant<internal::TireMF612> model_;
 };
 
 } // namespace tire_model
